bcf4a94332737704fb8b35bdde92012982823730
talcodota <tal@codota.com>
2020-12-01 17:04:30 +0200

    another

--- src/pages/layout-intrinsic.js
+++ src/pages/layout-intrinsic.js
@@ -1,19 +1,23 @@
-import Image from 'next/image'
-import ViewSource from '../components/view-source'
+import Image from 'next/image';
 
 const Intrinsic = () => (
-  // <div>
-  //   {/* <ViewSource pathname="pages/layout-intrinsic.js" /> */}
-  //   <h1>Image Component With Layout Intrinsic</h1>
+	<>
+		<Image
+			alt='Mountains'
+			src='/mountains.jpg'
+			layout='intrinsic'
+			width={700}
+			height={475}
+		/>
 
-  // </div>
-  <Image
-  alt="Mountains"
-  src="/mountains.jpg"
-  layout="intrinsic"
-  width={700}
-  height={475}
-/>
-)
+		<Image
+			alt='Mountains2'
+			src='/base/mountains.jpg'
+			layout='intrinsic'
+			width={700}
+			height={475}
+		/>
+	</>
+);
 
-export default Intrinsic
+export default Intrinsic;
